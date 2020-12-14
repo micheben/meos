b5a7887770de78284385059e953cec957d17e9bc
markus.mauer <markus.mauer@inworks.de>
2020-12-14 22:51:41 +0100

    Meaty Skeleton Rename

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/meos.kernel isodir/boot/meos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "meos" {
+	multiboot /boot/meos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o meos.iso isodir
