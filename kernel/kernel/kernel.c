c3cfeaf7107c5b344f0a0e0fa7cdee1d135a4180
markus.mauer <markus.mauer@inworks.de>
2020-12-22 14:12:42 +0100

    GDT & IDT added

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 
 #include <kernel/tty.h>
@@ -10,6 +11,27 @@ __attribute__ ((constructor)) void _DummyGloablCConstructor(void) {
 	printf("_DummyGloablCConstructor is running and printf is available at this point\n");
 }
 
+
+typedef struct registers
+{
+   uint32_t ds;                  // Data segment selector
+   uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; // Pushed by pusha.
+   uint32_t int_no, err_code;    // Interrupt number and error code (if applicable)
+   uint32_t eip, cs, eflags, useresp, ss; // Pushed by the processor automatically.
+} registers_t;
+
+// This gets called from our ASM interrupt handler stub.
+void isr_handler()
+{
+	printf("recieved interrupt: \n");
+}
+
+
 void kernel_main(void) {
 	printf("Hello, kernel World!\n");
+	__asm__ __volatile__ ("int $0x3");
+
+
+	printf("Done...!\n");
+
 }
